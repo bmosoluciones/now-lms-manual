0b059bfb054381033799af2aa16d056b0df314d0
William José Moreno Reyes <williamjmorenor@gmail.com>
2023-01-31 12:44:06 -0600

    Setup
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NOW - LMS User Manual'
+copyright = '2023, BMO Soluciones, S.A.'
+author = 'BMO Soluciones, S.A.'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extension = ["sphinx_immaterial"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_immaterial"
+html_static_path = ['_static']
